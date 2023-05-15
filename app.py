9f5dee70d2c972854781e34da410414ff68b1ca6
sang459 <sang459@gmail.com>
2023-05-15 21:25:56 +0900

    DETAILED 추가

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def process_and_convert(files):
 
 def summary(raw_text):
     # raw_text를 split하기
-    text_splitter = TokenTextSplitter(chunk_size=3000, chunk_overlap=30)
+    text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=30)
     texts = text_splitter.split_text(raw_text)
     print(len(texts))
     print(texts[1])
@@ -76,13 +76,13 @@ def summary(raw_text):
     # summarize chain 생성 및 실행 (prompt 길이 증가율이 양수)
     # 지금으로선 문서 길이가 너무 길어서 summary가 약 6000토큰 넘어가면 overflow되는 문제 있음
     # 또 다른 방법: chat history 사용하기? (scope 설정하면 비용증가 조절 가능할듯)
-    prompt_template = """FULL TEXT: 
+    prompt_template = """FULL TEXT:
 
 
     {text}
 
 
-    SUMMARY WITH IMPORTANT DETAILS (using markdown headings and bulletpoints):"""
+    DETAILED SUMMARY (using markdown headings and bullet points):"""
     PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
     refine_template = (
         "Your job is to produce a summary of the article.\n"
@@ -91,15 +91,14 @@ def summary(raw_text):
         "------------\n"
         "{text}\n"
         "------------\n"
-        "Produce a detailed summary of the given part of the article, using headings and bullet points.\n"
-        "Omit the overlapping part of the summary."
+        "Produce a detailed summary of the given part of the article, using markdown headings and bullet points.\n" # detailed가 중요한듯!
     )
     refine_prompt = PromptTemplate(
         input_variables=["existing_answer", "text"],
         template=refine_template,
     )
     # 한 chunk씩 썸머리하고 합쳐서 반환
-    chain = load_summarize_chain(OpenAI(temperature=0.1, openai_api_key=OPENAI_API_KEY), chain_type="refine", 
+    chain = load_summarize_chain(OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY), chain_type="refine", 
                                  return_intermediate_steps=True, question_prompt=PROMPT, 
                                  refine_prompt=refine_prompt)
     summarizations = chain({"input_documents": docs}, return_only_outputs=True)['intermediate_steps']
